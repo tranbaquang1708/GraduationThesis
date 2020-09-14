645f7e16b4801fe0fbddf7889a0f6d1505ea208e
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2020-09-15 01:13:27 +0900

    Re-organize code

--- modules/IGR.py
+++ modules/IGR.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 import sys
 import os
-from modules import Distance, Visualization
+from modules import Distance, Visualization, Distribution
 
 # Neural network model
 class IGRPerceptron(nn.Module):
@@ -54,6 +54,20 @@ class LossFunction:
       self.ld = ld
 
     self.distribution = distribution
+
+  def eval_distribution(self, model, batch_points, device):
+    d = self.distribution(model, batch_points, device)
+    # Visualization.scatter_plot(d.detach().cpu().numpy())
+    # print(d)
+    x = torch.autograd.Variable(d, requires_grad=True)
+    x.to(device)
+    f = model(x)
+    g = torch.autograd.grad(outputs=f, inputs=x, 
+                    grad_outputs=torch.ones(f.size()).to(device), 
+                    create_graph=True, retain_graph=True, 
+                    only_inputs=True)[0]
+
+    return (((g.norm(2, dim=1) - 1))**2).mean()
   
   # Compute loss
   def irg_loss(self, model, result, batch_points, batch_normal_vectors, device):
@@ -71,7 +85,7 @@ class LossFunction:
     if self.distribution is None:
       constrain = 0
     else:
-      constrain = constrain_function(model, batch_points, device)
+      constrain = self.eval_distribution(model, batch_points, device)
 
     return geo_loss + self.tau*grad_loss + self.ld * constrain
 
@@ -92,12 +106,6 @@ def to_batch(dataset, batch_size):
   # print(batches_indices)
   return batches_indices
 
-  # if len(dataset) > batch_size:
-  #   [:batch_size]
-  #   return dataset[indices], normal_vectors[indices]
-  # else:
-  #   return dataset, normal_vectors
-
 def train(dataset, normal_vectors, num_epochs, batch_size, device, loss_function, model=None):
   if model is None:
     model = IGRPerceptron()
@@ -106,6 +114,9 @@ def train(dataset, normal_vectors, num_epochs, batch_size, device, loss_function
   optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
   loss = 999.0
+
+  loss_i = []
+  loss_value = []
 
   for i in range(num_epochs):
     batches_indices = to_batch(dataset, batch_size)
@@ -125,61 +136,17 @@ def train(dataset, normal_vectors, num_epochs, batch_size, device, loss_function
       # Visualization.scatter_plot(batch_points.detach())
       
     if (i+1)%500 == 0:
+      loss_i.append(i+1)
+      loss_value.append(loss)
       print("Step " + str(i+1) + ":")
       print(loss)
 
     if num_epochs == 1:
       print(loss)
+
+  Visualization.loss_graph(loss_i, loss_value)
     
   return model
-
-# Uniform distribution
-# def uniform_distribution(model, batch_points, device):
-#   xmin = torch.min(batch_points[:,0]).item()
-#   xmax = torch.max(batch_points[:,0]).item()
-#   ymin = torch.min(batch_points[:,1]).item()
-#   ymax = torch.max(batch_points[:,1]).item()
-
-#   dist_min = torch.ones(batch_points.size()).to(device)
-#   dist_min[:,0] = dist_min[:,0] * xmin
-#   dist_min[:,1] = dist_min[:,1] * ymin
-
-#   dist_max = torch.ones(batch_points.size()).to(device)
-#   dist_max[:,0] = dist_max[:,0] * xmax
-#   dist_max[:,1] = dist_max[:,1] * ymax
-
-#   uniform = torch.distributions.uniform.Uniform(dist_min, dist_max)
-#   dist = uniform.sample().to(device)
-
-#   x = torch.autograd.Variable(dist, requires_grad=True)
-#   x.to(device)
-#   f = model(x)
-#   g = torch.autograd.grad(outputs=f, inputs=x, 
-#                     grad_outputs=torch.ones(f.size()).to(device), 
-#                     create_graph=True, retain_graph=True, 
-#                     only_inputs=True)[0]
-
-#   return (((g.norm(2, dim=1) - 1))**2).mean()
-
-# def eikonal_term(model, batch_points, device):
-#   # Uniform distribution
-#   xmin = torch.min(batch_points[:,0]).item()
-#   xmax = torch.max(batch_points[:,0]).item()
-#   ymin = torch.min(batch_points[:,1]).item()
-#   ymax = torch.max(batch_points[:,1]).item()
-
-#   dist_min = torch.ones(batch_points.size()).to(device)
-#   dist_min[:,0] = dist_min[:,0] * xmin
-#   dist_min[:,1] = dist_min[:,1] * ymin
-
-#   dist_max = torch.ones(batch_points.size()).to(device)
-#   dist_max[:,0] = dist_max[:,0] * xmax
-#   dist_max[:,1] = dist_max[:,1] * ymax
-
-#   uniform = torch.distributions.uniform.Uniform(dist_min, dist_max)
-#   uniform_dist = uniform.sample().to(device)
-
-  # Sum of Gaussian
   
 # Save trained data
 def save_model(path, model):
