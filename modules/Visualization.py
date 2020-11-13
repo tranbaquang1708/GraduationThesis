0ec186f8bbcdc2565eacbfdb3439498c3c77f774
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2020-11-13 10:31:26 +0900

    Remove print out for sampling

--- modules/Visualization.py
+++ modules/Visualization.py
@@ -8,38 +8,51 @@ import numpy as np
 #-------------------------------------------------------
 # PLOT
 
+# Scatter plot
+def scatter_plot(points):
+  if points.shape[1] == 2:
+    h_points = plt.scatter(points[:,0], points[:,1], s=2)
+    plt.show()
+  else:
+    fig = plt.figure(figsize=(10,10))
+    ax_points = fig.add_subplot(111, projection='3d')
+    ax_points.scatter(points[:,0], points[:,1], points[:,2])
+    plt.show()
+
+# Line graph for loss value
+def loss_graph(i, value):
+  plt.plot(i, value)
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss value')
+  plt.title('Loss Value')
+  plt.show()
+
 # Visualization for 2D dataset
 def visualize2(dataset, normal_vectors, xx, yy, z, scatter=True, vecfield=True, surface=True, filled_contour=True):
   # Visualize for 2D
   # Scatter plot for points
   if scatter:
     h_points = plt.scatter(dataset[:,0], dataset[:,1], s=2)
+    plt.title('Point-cloud')
     plt.show()
   # Plot vector field
   if vecfield:
     h_vector = plt.quiver(dataset[:,0], dataset[:,1], normal_vectors[:,0], normal_vectors[:,1])
     h_vector.ax.axis('equal')
+    plt.title('Normal Vectors')
     plt.show()
   # Plot generated surface
   if surface:
     h_object = plt.contour(xx,yy, z, levels=[0.0], colors='c')
     h_object.ax.axis('equal')
+    plt.title('Contour Plot')
     plt.show()
   # Plot filled contour
   if filled_contour:
     hf = plt.contourf(xx,yy,z)
     hf.ax.axis('equal')
+    plt.title('Filled Contour Plot')
     plt.show()
-
-def scatter_plot(points):
-  h_points = plt.scatter(points[:,0], points[:,1], s=2)
-  plt.show()
-
-def loss_graph(i, value):
-  plt.plot(i, value)
-  plt.xlabel('Epoch')
-  plt.ylabel('Loss value')
-  plt.show()
 
 # Visualization for 3D dataset
 def visualize3(dataset, normal_vectors, z, scatter=True, vecfield=True, surface=True):
@@ -56,6 +69,7 @@ def visualize3(dataset, normal_vectors, z, scatter=True, vecfield=True, surface=
     plt.show()
   if surface:
     verts, faces, normals, values = measure.marching_cubes_lewiner(z, 0)
+    print(faces)
     fig = plt.figure(figsize=(10,10))
     ax_surface = fig.add_subplot(111, projection='3d')
     mesh = Poly3DCollection(verts[faces])
@@ -109,7 +123,7 @@ def grid_from_torch(X, Y, Z=None, resx=50, resy=50, resz=50, device='cpu'):
 # SAMPLING
 
 # Neural Network as function
-def nn_sampling(model, xx, yy, zz=None, g_norm_output_path=None, device='cpu'):
+def nn_sampling(model, xx, yy, zz=None, value_output_path=None, g_norm_output_path=None, device='cpu'):
   # Evaluate function on each grid point
   resx = xx.shape[0]
   resy = yy.shape[0]
@@ -124,34 +138,28 @@ def nn_sampling(model, xx, yy, zz=None, g_norm_output_path=None, device='cpu'):
     tt = torch.reshape(tt, (dimg,3))
   
   z = model(tt)
+
+  # Save z value
+  if value_output_path is not None:
+    np.savetxt(value_output_path, z.detach().cpu())
+    print("Value saved")
+
   if zz is None:
-    print(torch.reshape(z, (resx,resy))) 
+    z = torch.reshape(z, (resx,resy))
   else: 
-    print(torch.reshape(z, (resx,resy, resz)))
-
-  # Compute grad on each grid point
-  x = torch.autograd.Variable(tt, requires_grad=True)
-  x = x.to(device)
-  f = model(x)
-  g = torch.autograd.grad(outputs=f, inputs=x, 
-                      grad_outputs=torch.ones(f.size()).to(device), 
-                      create_graph=True, retain_graph=True, 
-                      only_inputs=True)[0]
-
-  print()
-  print("Grad on each grid point")
-  g = torch.norm(g, dim=-1)
-
-  min_g = torch.min(g).item()
-  max_g = torch.max(g).item()
-  print("Minimum value: " + str(min_g))
-  print("Maximum value: " + str(max_g))
+    z = torch.reshape(z, (resx,resy, resz))
 
   if g_norm_output_path is not None:
+    # Compute grad on each grid point
+    x = torch.autograd.Variable(tt, requires_grad=True)
+    x = x.to(device)
+    f = model(x)
+    g = torch.autograd.grad(outputs=f, inputs=x, 
+                        grad_outputs=torch.ones(f.size()).to(device), 
+                        create_graph=True, retain_graph=True, 
+                        only_inputs=True)[0]
+
     np.savetxt(g_norm_output_path, g.detach().cpu())
     print("Norm of gradient saved")
 
-  if zz is None:
-    return torch.reshape(z, (resx,resy))
-  else:
-    return torch.reshape(z, (resx, resy, resz))
+  return z
