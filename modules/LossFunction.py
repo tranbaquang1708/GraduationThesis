c754db1bcb84f36d00f124ba9c2b359b930806d9
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2020-11-13 10:33:04 +0900

    Add standard deviation for Gaussian

--- modules/LossFunction.py
+++ modules/LossFunction.py
@@ -1,23 +1,18 @@
 import torch
 import numpy
-from modules import Visualization
+from modules import Visualization, Distribution
 from scipy import spatial
 
 class IGRLoss:
-  def __init__(self, distribution=None):
+  def __init__(self, points=None, distribution=None):
     self.tau = 1
     self.ld = 0.01
     self.distribution = distribution
+    if points is not None:
+      self.stddvt = Distribution.find_kth_closest_distance(points, 50)
 
   def eval_distribution(self, model, batch_points, device='cpu'):
-    # d = self.distribution(batch_points, device)
-
-    # Randomly sample from ditribution
-    # r = random.sample(range(self.distribution.shape[0]), batch_points.shape[0])
-    # d = self.distribution[r]
-
-    # Use all from the distribution
-    d = self.distribution
+    d = self.distribution(batch_points, batch_points.shape[0], self.stddvt, device)
 
     # Visualization.scatter_plot(d.detach().cpu().numpy())
     # print(d)
@@ -43,7 +38,6 @@ class IGRLoss:
                       create_graph=True, retain_graph=True, 
                       only_inputs=True)[0]
     grad_loss = (g - batch_normal_vectors).norm(2, dim=1).mean()
-    # grad_loss = 0
 
     if self.distribution is None:
       constrain = 0
@@ -58,7 +52,6 @@ class RBFLoss:
     self.distribution = distribution
 
   def compute_loss(self, model, result, batch_points, batch_normal_vectors, device):
-    # onsurface_loss = torch.mean(torch.abs(result))
     onsurface_loss = (result.abs()).mean()
     
     tree = spatial.KDTree(batch_points.detach().cpu().numpy())
@@ -88,13 +81,7 @@ class IGRBFLoss:
     self.distribution = distribution
 
   def eval_distribution(self, model, batch_points, device='cpu'):
-    # d = self.distribution(batch_points, device)
 
-    # Randomly sample from ditribution
-    # r = random.sample(range(self.distribution.shape[0]), batch_points.shape[0])
-    # d = self.distribution[r]
-
-    # Use all from the distribution
     d = self.distribution
 
     # Visualization.scatter_plot(d.detach().cpu().numpy())
