6518eba28438adc4e1ff682be6534623b87c1b9e
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2020-12-03 11:42:19 +0900

    Add export to .vtk file option

--- modules/Visualization.py
+++ modules/Visualization.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import torch
 import math
 import numpy as np
+from modules import Operation
 
 #-------------------------------------------------------
 # PLOT
@@ -32,23 +33,27 @@ def visualize2(dataset, normal_vectors, xx, yy, z, scatter=True, vecfield=True, 
   # Visualize for 2D
   # Scatter plot for points
   if scatter:
-    h_points = plt.scatter(dataset[:,0], dataset[:,1], s=2)
+    plt.figure(figsize=(8,4))
+    h_points = plt.scatter(dataset[:,0], dataset[:,1], s=5)
     plt.title('Point-cloud')
     plt.show()
   # Plot vector field
   if vecfield:
+    plt.figure(figsize=(8,4))
     h_vector = plt.quiver(dataset[:,0], dataset[:,1], normal_vectors[:,0], normal_vectors[:,1])
     h_vector.ax.axis('equal')
     plt.title('Normal Vectors')
     plt.show()
   # Plot generated surface
   if surface:
+    plt.figure(figsize=(8,4))
     h_object = plt.contour(xx,yy, z, levels=[0.0], colors='c')
     h_object.ax.axis('equal')
     plt.title('Contour Plot')
     plt.show()
   # Plot filled contour
   if filled_contour:
+    plt.figure(figsize=(8,4))
     hf = plt.contourf(xx,yy,z)
     hf.ax.axis('equal')
     plt.title('Filled Contour Plot')
@@ -69,7 +74,6 @@ def visualize3(dataset, normal_vectors, z, scatter=True, vecfield=True, surface=
     plt.show()
   if surface:
     verts, faces, normals, values = measure.marching_cubes_lewiner(z, 0)
-    print(faces)
     fig = plt.figure(figsize=(10,10))
     ax_surface = fig.add_subplot(111, projection='3d')
     mesh = Poly3DCollection(verts[faces])
@@ -85,16 +89,18 @@ def visualize3(dataset, normal_vectors, z, scatter=True, vecfield=True, surface=
 # GRID
 
 # Create a grid from torch tensot
-def grid_from_torch(X, Y, Z=None, resx=50, resy=50, resz=50, device='cpu'):
-  xmin = torch.min(X).item()
-  xmax = torch.max(X).item()
-  ymin = torch.min(Y).item()
-  ymax = torch.max(Y).item()
+def grid_from_torch(points, resx=50, resy=50, resz=50, device='cpu'):
+  dims = points.shape[1]
+
+  xmin = torch.min(points[:,0]).item()
+  xmax = torch.max(points[:,0]).item()
+  ymin = torch.min(points[:,1]).item()
+  ymax = torch.max(points[:,1]).item()
 
   dx = xmax - xmin
   dy = ymax - ymin
 
-  if Z is None: # 2D case
+  if dims == 2: # 2D case
     ed = 0.1*math.sqrt(dx*dx+dy*dy)
 
     x = torch.arange(xmin-ed, xmax+ed, step=(dx+2*ed)/float(resx))
@@ -104,8 +110,8 @@ def grid_from_torch(X, Y, Z=None, resx=50, resy=50, resz=50, device='cpu'):
 
     return xx.to(device), yy.to(device)
   else: # 3D case
-    zmin = torch.min(Z).item()
-    zmax = torch.max(Z).item()
+    zmin = torch.min(points[:,2]).item()
+    zmax = torch.max(points[:,2]).item()
 
     dz = zmax - zmin
 
@@ -123,26 +129,24 @@ def grid_from_torch(X, Y, Z=None, resx=50, resy=50, resz=50, device='cpu'):
 # SAMPLING
 
 # Neural Network as function
-def nn_sampling(model, xx, yy, zz=None, value_output_path=None, g_norm_output_path=None, device='cpu'):
+def nn_sampling(model, xx, yy, zz=None, vtk_output_path=None, g_norm_output_path=None, device='cpu'):
   # Evaluate function on each grid point
   resx = xx.shape[0]
   resy = yy.shape[0]
   if zz is None:
     dimg = resx * resy
-    tt = torch.stack((xx, yy), axis=2)
-    tt = torch.reshape(tt, (dimg,2))
+    tt = torch.stack((xx, yy), dim=-1).reshape(dimg,2)
   else:
     resz = zz.shape[0]
     dimg = resx * resy * resz
-    tt = torch.stack((xx, yy, zz), axis=3)
-    tt = torch.reshape(tt, (dimg,3))
+    tt = torch.stack((xx, yy, zz), dim=-1).reshape(dimg,3)
   
   z = model(tt)
 
   # Save z value
-  if value_output_path is not None:
-    np.savetxt(value_output_path, z.detach().cpu())
-    print("Value saved")
+  if vtk_output_path is not None:
+    Operation.save_vtk(vtk_output_path, tt, resx, resy, resz, z)
+    print("VTK file saved")
 
   if zz is None:
     z = torch.reshape(z, (resx,resy))
