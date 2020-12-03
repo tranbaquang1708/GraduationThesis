c0a4b4c4522414ef43be29a28518e5255a822bb1
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2020-12-03 11:38:29 +0900

    Make uniform distribution the same as authors'

--- modules/Distribution.py
+++ modules/Distribution.py
@@ -6,33 +6,36 @@ from modules import Visualization
 
 # Uniform distribution
 def uniform(points, dist_size, device='cpu'):
-  xmin = torch.min(points[:,0]).item()
-  xmax = torch.max(points[:,0]).item()
-  ymin = torch.min(points[:,1]).item()
-  ymax = torch.max(points[:,1]).item()
+  # xmin = torch.min(points[:,0]).item()
+  # xmax = torch.max(points[:,0]).item()
+  # ymin = torch.min(points[:,1]).item()
+  # ymax = torch.max(points[:,1]).item()
 
-  # dist_min = torch.ones(points.size()).to(device)
-  dist_min = torch.ones((dist_size, points[0].shape[0])).to(device)
-  dist_min[:,0] = dist_min[:,0] * xmin
-  dist_min[:,1] = dist_min[:,1] * ymin
+  # # dist_min = torch.ones(points.size()).to(device)
+  # dist_min = torch.ones((dist_size, points[0].shape[0])).to(device)
+  # dist_min[:,0] = dist_min[:,0] * xmin
+  # dist_min[:,1] = dist_min[:,1] * ymin
 
-  # dist_max = torch.ones(points.size()).to(device)
-  dist_max = torch.ones((dist_size, points[0].shape[0])).to(device)
-  dist_max[:,0] = dist_max[:,0] * xmax
-  dist_max[:,1] = dist_max[:,1] * ymax
+  # # dist_max = torch.ones(points.size()).to(device)
+  # dist_max = torch.ones((dist_size, points[0].shape[0])).to(device)
+  # dist_max[:,0] = dist_max[:,0] * xmax
+  # dist_max[:,1] = dist_max[:,1] * ymax
 
-  if points[0].shape[0] == 3:
-    zmin = torch.min(points[:,2]).item()
-    zmax = torch.max(points[:,2]).item()
-    dist_min[:,2] = dist_min[:,2] * zmin
-    dist_max[:,2] = dist_max[:,2] * zmax
+  # if points[0].shape[0] == 3:
+  #   zmin = torch.min(points[:,2]).item()
+  #   zmax = torch.max(points[:,2]).item()
+  #   dist_min[:,2] = dist_min[:,2] * zmin
+  #   dist_max[:,2] = dist_max[:,2] * zmax
 
-  uniform = torch.distributions.uniform.Uniform(dist_min, dist_max)
-  dist = uniform.sample().to(device)
+  # uniform = torch.distributions.uniform.Uniform(dist_min, dist_max)
+  # dist = uniform.sample().to(device)
+
+  global_sigma = 1.8
+  dist = (torch.rand(dist_size, points.shape[1]) * 2 - 1) * global_sigma 
 
   return dist
 
-def dense_uniform(points, dist_size, device='cpu'):
+def dense_uniform(points, dist_size):
   half1 = int(points.shape[0]/2)
   half2 = points.shape[0] - half1
 
@@ -63,7 +66,7 @@ def dense_uniform(points, dist_size, device='cpu'):
   dist_max = torch.cat((dist_max1, dist_max2))
 
   uniform = torch.distributions.uniform.Uniform(dist_min, dist_max)
-  dist = uniform.sample().to(device)
+  dist = uniform.sample()
 
   return dist
 
@@ -72,19 +75,19 @@ def find_kth_closest_distance(points, k):
   d_50 = tree.query(points.detach().cpu().numpy(), k=k)[0][:,-1]
   stddvt = torch.from_numpy(d_50).reshape((points.size()[0], 1))
   
-  return stddvt.type(torch.cuda.FloatTensor)
+  return stddvt.type(torch.FloatTensor)
 
-def gaussian(points, dist_size, stddvt, device='cpu'):
+def gaussian(points, stddvt):
   dist = points + (torch.randn_like(points, dtype=torch.float32) * stddvt)
   return dist
 
 # The average of a uniform distribution and a sum of Gaussians centered 
 # at X with standard deviation equal to the distance to the k-th nearest 
 # neighbor (we used k = 50)
-def uniform_gaussian(points, dist_size, stddvt, device='cpu'):
-  u = uniform(points, dist_size//8, device)
-  g = gaussian(points, dist_size, stddvt, device)
+def uniform_gaussian(points, dist_size, sample_range, stddvt):
+  # g = gaussian(points, stddvt)
+  # u = uniform(points, dist_size // 8)#.to(g.device)
+  u = (torch.rand(dist_size//8, points.shape[1], device=points.device) * 2 - 1) * sample_range
+  g = points + (torch.randn_like(points, dtype=torch.float32) * stddvt)
 
-  u_g = torch.cat((u,g))
-  
-  return u_g
+  return torch.cat((u,g))
