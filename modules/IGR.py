52cb86208c1ae027e7949dde59fdd37519530bf4
tranbaquang1708 <37550326+tranbaquang1708@users.noreply.github.com>
2021-01-13 21:52:20 +0900

    Add scheduler, Laplacian

--- modules/IGR.py
+++ modules/IGR.py
@@ -9,50 +9,44 @@ import math
 import random
 import sys
 import os
-from modules import Distance, Visualization, Distribution, Operation
+from modules import Distance, Visualization, Operation
 
 
-def train(num_iters, model, optimizer, batch_size=None, data_file=None, points=None, normal_vectors=None, loss_output_path=None, device='cpu'):
+def train(num_iters, model, optimizer, scheduler, p=2, batch_size=None, data_file=None, points=None, normal_vectors=None, loss_output_path=None, device='cpu'):
   if data_file is not None:
     model = train_file(num_iters, model, optimizer, batch_size, data_file, loss_output_path, device)
   if points is not None:
-    model = train_data(num_iters, model, optimizer, batch_size, points, normal_vectors, loss_output_path, device)
+    model, optimizer, scheduler = train_data(num_iters, model, optimizer, scheduler, p, batch_size, points, normal_vectors, loss_output_path, device)
 
-  return model
+  return model, optimizer, scheduler
 
 
 # Train with data passed
-def train_data(num_iters, model, optimizer, batch_size, points, normal_vectors, loss_output_path=None, device='cpu'):
+def train_data(num_iters, model, optimizer, scheduler, p, batch_size, points, normal_vectors, loss_output_path=None, device='cpu'):
   print('Setting up')
 
   lb = 1.0
-  tau = 0.1
+  tau = 0.5
 
   loss = 0
   loss_checkpoint_freq = 100
   plot_freq = 100
 
-  lr_adjust_interval = 2000
-  lr_initial = 0.001
-  lr_factor = 0.25
-  lr_min = 1.0e-7
+  # lr_adjust_interval = 2000
+  # lr_initial = 0.005
+  # lr_factor = 0.5
+  # lr_min = 5.0e-6
 
   # Get loss values and number of iteration in last training
   loss_value, start = Operation.load_loss_values(loss_output_path)
 
   # Standard deviation for Gaussian
-  print('Getting distances to 50th neighbor')
-  stddvt = Distribution.find_kth_closest_distance(points, 50).to(device)
+  print('Getting distance to 50th closest neighbor')
+  stddvt = Operation.find_kth_closest_d(points, 50)
+  
 
-  print('Getting sample range')
-  sample_range = (points.abs().max() + 1) * 1.1
-
-  # Grid for plot result
-  if points.shape[1] == 2:
-    print('Creating grid')
-    xx, yy = Visualization.grid_from_torch(points, resx=50, resy=50, device=device)
-  # else:
-  #   xx, yy, zz = Visualization.grid_from_torch(points, resx=50, resy=50, resz=50, device=device)
+  print('Getting sampling range')
+  sample_ranges = Operation.get_sample_ranges(points)
   
    
   # No batch_size value passed, use all data every iteration
@@ -60,106 +54,71 @@ def train_data(num_iters, model, optimizer, batch_size, points, normal_vectors, 
     batch_size = points.shape[0]
 
 
-  # Change to train mode
-  # model.train()
-
   # Train network
   print()
   print('Training')
   for i in range(start, start+num_iters):
-    
-
-    # Change to train mode
-    model.train()
-
 
     # Seperate data into batches
     # index_batches = list(Operation.chunks(random.sample(range(points.shape[0]), points.shape[0]), batch_size))
     # for index_batch in index_batches:
     index_batch = np.random.choice(points.shape[0], batch_size, False)
     point_batch = points[index_batch]
+    # normal_batch = normal_vectors[index_batch]
 
 
     # Distribution
-    distribution = Distribution.uniform_gaussian(point_batch, len(index_batch), sample_range, stddvt[index_batch]).to(device)
+    # distribution = Operation.uniform_gaussian(point_batch, batch_size, sample_ranges, stddvt[index_batch])
+    distribution = Operation.uniform_gaussian(point_batch, batch_size, sample_ranges, stddvt[index_batch])
     # Visualization.scatter_plot(distribution.detach().cpu().numpy())
-    
 
-    # Adjust learning rate
-    if (i) % lr_adjust_interval == 0:
-      for i_param, param_group in enumerate(optimizer.param_groups):
-        param_group["lr"] = np.maximum(lr_initial * (lr_factor ** (i // lr_adjust_interval)) , lr_min)
+    # Change to train mode
+    model.train()
+
+    
+    optimizer.zero_grad()
 
     
     # Compute loss
     # TotalLoss = f(x) + (grad(f)-normals) + constraint
 
+    # Forward pass
+    f_data = model(point_batch)
+    # f_sample = model(distribution)
+
     # f(x)
     # geo_loss = model(points[index_batch]).abs().mean()
-    geo_loss = model(point_batch).abs().mean()
+    geo_loss = (f_data.abs()).mean()
+
+    # print(f_data-model(point_batch))
 
     # grad(f)-normals
-    g = Operation.compute_grad(point_batch, model)
-    grad_loss = lb * (g - normal_vectors[index_batch]).norm(2, dim=1).mean()
+    normal_grad = Operation.compute_grad(point_batch, f_data)
+    grad_loss = ((normal_grad - normal_vectors[index_batch]).abs()).norm(2, dim=1).mean()
 
     # Constraint
-    # Eikonal term: grad(f)-1
-    g = Operation.compute_grad(distribution, model)
-    constraint = tau * (((g.norm(2, dim=1) - 1))**2).mean()
+    constraint_grad = Operation.laplacian(model, distribution, p=p)
+    constraint = ((constraint_grad - 1.)**2).mean()
 
-    loss = geo_loss + grad_loss + constraint
+    loss = geo_loss + lb * grad_loss + tau * constraint
     
 
-    optimizer.zero_grad()
     loss.backward(retain_graph=True)
     optimizer.step()
-    
-    
-    # Plot results
-    if (i+1) % plot_freq == 0:
-      # model = model.eval()
-      # 2d plot
-      if (points.shape[1] == 2):
-        z = Visualization.nn_sampling(model, xx, yy, device=device)
-        try:
-          plt.figure(figsize=(12,3))
-          plt.subplot(1, 2, 1)
-          h = plt.contour(xx.detach().cpu(),yy.detach().cpu(), z.detach().cpu(), levels=[0.0], colors='c')
-
-          plt.subplot(1, 2, 2)
-          hf = plt.contourf(xx.detach().cpu(),yy.detach().cpu(), z.detach().cpu())
-          plt.show()
-        except:
-          print('No result')
-
-      # else: # 3D plot
-      #   z = Visualization.nn_sampling(model, xx, yy, zz, g_norm_output_path=None, device=device)
-      #   try:
-      #     verts, faces, normals, values = measure.marching_cubes_lewiner(z.detach().cpu().numpy(), 0)
-      #     fig = plt.figure(figsize=(5,5))
-      #     ax_surface = fig.add_subplot(111, projection='3d')
-      #     mesh = Poly3DCollection(verts[faces])
-      #     mesh.set_edgecolor('k')
-      #     ax_surface.add_collection3d(mesh)
-      #     ax_surface.set_xlim(0, 50)
-      #     ax_surface.set_ylim(0, 50)
-      #     ax_surface.set_zlim(0, 50)
-      #     plt.tight_layout()
-      #     plt.show()
-      #   except:
-          # print('No result')
+    scheduler.step()
 
 
     # Store loss values into numpy array
     if (i+1) % loss_checkpoint_freq == 0:
-      loss_value = np.append(loss_value, [[i+1, loss.item()]], axis=0)
+      loss_value = np.append(loss_value, [[i+1, loss.item(), geo_loss.item(), grad_loss.item(), tau * constraint.item()]], axis=0)
+      # print((constraint_grad < 0).sum())
       print('Iteration:', i+1, '  Loss:', loss.item(), '  Learning rate:', optimizer.param_groups[0]["lr"])
       print('Surface loss:' , geo_loss.item(), '  Normal loss:', grad_loss.item(), '  Constraint:', constraint.item())
       print()
 
 
   # Draw figure for loss values
-  Visualization.loss_graph(loss_value[:,0], loss_value[:,1])
+  Visualization.loss_graph(loss_value[:,0], loss_value[:,-4:])
 
 
   # Save numpy array of loss values
@@ -169,7 +128,7 @@ def train_data(num_iters, model, optimizer, batch_size, points, normal_vectors, 
   # Change to eval mode
   # model.eval()
 
-  return model, optimizer
+  return model, optimizer, scheduler
   
 
 # Train with file path passed
@@ -206,7 +165,7 @@ def train_file(num_iters, model, loss_function, batch_size, data_file, output_pa
     print(loss)
 
   # Plot line graph of loss values
-  Visualization.loss_graph(loss_value[:,0], loss_value[:,1])
+  Visualization.loss_graph(loss_value[:,0], loss_value[:,:-4])
 
   # Save loss value
   if output_path is not None:
@@ -217,4 +176,4 @@ def train_file(num_iters, model, loss_function, batch_size, data_file, output_pa
 
 def show_loss_figure(loss_path):
   loss_value = np.load(loss_path)
-  Visualization.loss_graph(loss_value[:,0], loss_value[:,1])
+  Visualization.loss_graph(loss_value[:,0], loss_value[:,-4:])
